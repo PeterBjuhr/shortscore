d931afc9d1dfc8b29b7a0208bc6f266fbdf4e9c0
PeterBjuhr <peterbjuhr@gmail.com>
2017-10-13 22:29:02 +0200

    Version 0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='shortscore',
-    version='0.1dev',
+    version='0.1',
     packages=['shortscore',],
     long_description=open('README.md').read(),
 )
