31802bcc26db610bd730293fb4bfc41869b708e7
PeterBjuhr <peterbjuhr@gmail.com>
2017-10-15 20:31:29 +0200

    Start dev towards next release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='shortscore',
-    version='0.1',
+    version='0.2dev',
     packages=['shortscore',],
     long_description=open('README.md').read(),
 )
