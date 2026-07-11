16ba5eff22febca94f4586447d8def7cde26d4d8
Peter Bjuhr <peterbjuhr@gmail>
2026-07-11 15:34:18 +0200

    feat: More clef improvements

--- shortscore/defaultClefs.py
+++ shortscore/defaultClefs.py
@@ -3,9 +3,12 @@ default_clefs = {
     "violas": "alto",
     "cello": "bass",
     "cellos": "bass",
+    "violoncello": "bass",
+    "violoncellos": "bass",
     "contrabass": "bass8D",
     "contrabasses": "bass8D",
     "bassoon": "bass",
     "contrabassoon": "bass8D",
+    "trombone": "bass",
     "timpani": "bass"
 }
